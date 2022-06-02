3112dba42389799a39dafd33d65482c565c6314f
Rafaela Queiroz <rqueiroz2108@gmail.com>
2022-06-02 20:44:12 -0300

    feat: :sparkles: adiciona navbar

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule, RouterModule],
+})
+export class CoreModule {}
