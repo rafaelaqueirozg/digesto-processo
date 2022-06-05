e670efd53b4a8c6a79a3a4a3426733d5945dd361
Rafaela Queiroz <rqueiroz2108@gmail.com>
2022-06-05 13:31:43 -0300

    feat: :sparkles: adiciona tipo standardizedDigestoType

---
+++ src/app/judicial-basis/types/standardized-digesto.type.ts
@@ -0,0 +1,3 @@
+export type StandardizedDigestoType = {
+  [key: string]: { [key: string]: string };
+};
---
+++ src/app/shared/utils/standardized-type-digesto.ts
@@ -0,0 +1,123 @@
+import { StandardizedDigestoType } from 'src/app/judicial-basis/types/standardized-digesto.type';
+
+export const STANDARDIZED_TYPE_DIGESTOO: StandardizedDigestoType = {
+  '0': { '0': 'Desconhecido' },
+  '1': { '1': 'Agravo de Instrumento Interposto' },
+  '2': {
+    '1': 'Audiência - Designada',
+    '2': 'Audiência - Redesignada',
+    '3': 'Audiência - Outros',
+    '4': 'Audiência - Cancelada',
+  },
+  '3': {
+    '1': 'Deferida - Citação',
+    '2': 'Deferida - conversao de busca e apreensão em execucao',
+    '3': 'Deferida - liminar recolhimento custas',
+    '4': 'Deferida - liminar teor',
+    '5': 'Deferido - arquivamento',
+    '6': 'Deferido - bloqueio bem',
+    '7': 'Deferido - pedido de penhora',
+    '8': 'Deferido - suspensão 921',
+    '9': 'Deferido - sobrestamento',
+    '10': 'Deferida - liminar em parte',
+  },
+  '4': {
+    '0': 'Manifestacao contestacao - outros',
+    '1': 'Contatar Of.Just. mandado deferido',
+    '2': 'Contestação manifestacao réu',
+  },
+  '5': { '1': 'Desbloqueado veiculo Detran' },
+  '6': {
+    '1': 'Extinção - sem Mérito I',
+    '2': 'Extinção - sem Mérito II',
+    '3': 'Extinção - sem Mérito III',
+    '4': 'Extinção - sem Mérito VI',
+    '5': 'Extinção - outros',
+  },
+  '7': {
+    '1': 'Homologada a desistência 485 VIII',
+    '2': 'Homologado acordo suspenso',
+    '3': 'Homologo acordo 487',
+  },
+  '8': { '1': 'Indeferida a Liminar' },
+  '9': { '1': 'Informa Núm.proc. e vara' },
+  '11': {
+    '1': 'Juntar - minuta de acordo',
+    '2': 'Juntar - planilha debito',
+  },
+  '12': { '1': 'Levantar deposito judicial' },
+  '13': {
+    '0': 'Manifestar nos autos - outros',
+    '1': 'Manifestar autor - Andamento Feito',
+    '2': 'Manifestar autor - Certidão Oficial de Justiça',
+  },
+  '14': {
+    '0': 'Manifestar provas - outros',
+    '1': 'Produção de provas',
+  },
+  '15': {
+    '1': 'Recolher custas - diligencia Of.Just.',
+    '2': 'Recolher custas - outros',
+  },
+  '16': { '1': 'Retirar oficios' },
+  '17': {
+    '1': 'Sentença - Procedente',
+    '2': 'Sentença - Improcedente',
+    '3': 'Sentença - Parcialmente procedente',
+  },
+  '18': { '1': 'Transitado em julgado' },
+  '19': {
+    '0': 'Manifestar oficio - outros',
+    '1': 'Verificar resposta oficios',
+  },
+  '20': {
+    '1': 'Iniciada - Liquidação',
+    '2': 'Iniciada - Execução',
+  },
+  '21': {
+    '0': 'Publicacao - outros',
+    '1': 'Publicação realizada',
+    '2': 'Expedição comunicação via sistema',
+  },
+  '22': {
+    '1': 'Expedido alvará ao réu',
+    '2': 'Alvará e deposito',
+  },
+  '23': {
+    '0': 'Disponibilizada intimacao - outros',
+    '1': 'Disponibilizada a Intimação',
+  },
+  '24': { '1': 'Expedida Notificação' },
+  '25': {
+    '0': 'Acórdão',
+    '1': 'Publicado o Acórdão',
+  },
+  '26': { '1': 'Conhecido o recurso - não provido' },
+  '27': { '1': 'Incluído em pauta' },
+  '28': { '1': 'Revelia' },
+  '29': {
+    '0': 'Manifestar reu - outros',
+    '1': 'Manifestar réu',
+  },
+  '30': {
+    '0': 'Expedida intimacao - outros',
+    '1': 'Citacao - Positiva',
+    '2': 'Citacao - Intimação expedida',
+    '3': 'Citacao - Citação expedida',
+  },
+  '31': { '2': 'Citacao - AR negativo juntado' },
+  '32': { '1': 'Prazo decorrido' },
+  '33': {
+    '1': 'Gratuidade - Deferida',
+    '2': 'Gratuidade - Indeferida',
+  },
+  '34': {
+    '1': 'Antecipação de Tutela - Concedida',
+    '2': 'Antecipação de Tutela - Não concedida',
+  },
+  '35': { '1': 'Carta precatória - Expedição' },
+  '36': { '1': 'Mandado - Expedição' },
+  '38': { '1': 'Ofício requisitorio' },
+  '39': { '1': 'Contadoria' },
+  '41': { '1': 'Decurso de prazo' },
+};
