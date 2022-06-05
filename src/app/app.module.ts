fb62184af6ed45937fa7f2b26c3b726e7a8cc35b
Rafaela Queiroz <rqueiroz2108@gmail.com>
2022-06-05 13:39:30 -0300

    feat: :globe_with_meridians: adiciona locale pt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { JudicialBasisModule } from './judicial-basis/judicial-basis.module';
 import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +21,12 @@ import { SharedModule } from './shared/shared.module';
     JudicialBasisModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
